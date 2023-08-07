e02d8ec502c281ac891eaf5d3a647c9d99266d84
csarsnchz <csarsnchz@gmail.com>
2023-08-07 17:18:01 -0600

    Update myApp.js
    Removed unnecesary variable

--- myApp.js
+++ myApp.js
@@ -123,14 +123,13 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  let personFound = Person.find({favoriteFoods: foodToSearch});
-  Person.find({favoriteFoods: foodToSearch})
-  .sort({name: 1})
-  .limit(2)
-  .select({name: 1, age: 0, favoriteFoods: 1})
-  .exec((err, people) =>{
-    if (err) return console.log(err);
-    done(null, people);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ name: 1, age: 0, favoriteFoods: 1 })
+    .exec((err, people) => {
+      if (err) return console.log(err);
+      done(null, people);
     });
 };
 

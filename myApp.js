333cb9907fa0297fc297e5258ab2d2e780509cc3
csarsnchz <csarsnchz@gmail.com>
2023-08-06 20:48:57 -0600

    Update myApp.js
    Changed variable name

--- myApp.js
+++ myApp.js
@@ -87,11 +87,12 @@ const findEditThenSave = (personId, done) => {
   Person.findById({_id: personId}, (err, personById) => {
     if (err) return console.log(err);
     personById.favoriteFoods.push(foodToAdd);
-    personById.save(function(err, data) {
+    personById.save(function(err, personUpdated) {
     if (err) return console.error(err);
-    done(null, data)
+    done(null, personUpdated)
     });
     console.log(personById);
+    console.log(personUpdated);
     }); 
 };
 
